2036f95f7a49f3e48ce5193fc75654384cccce83
sida wang <sida.wang.cs@gmail.com>
2016-04-27 14:00:42 -0700

    Initial Commit

---
+++ WandaHealthTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WandaHealthTracker
+//
+//  Created by sidawang on 4/27/16.
+//  Copyright © 2016 sidex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WandaHealthTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WandaHealthTracker
+//
+//  Created by sidawang on 4/27/16.
+//  Copyright © 2016 sidex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
